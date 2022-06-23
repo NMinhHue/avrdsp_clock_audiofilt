1f21074283ebde748ad87d521c728d14d60252f3
NMinhHue <lethienkydu@gmail.com>
2022-06-23 15:43:26 +0700

    update

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,497 +1,244 @@
-/*
- * main.c
- *
- *  Created on: 24 May 2022
- *      Author: Tien Nguyen
- */
-
-#include "i2c.h"
-#include "ds1307.h"
+#define F_CPU7372800_UL
 #include <avr/io.h>
-#include <avr/iom128.h>
-#include <stdint.h>
-#include <util/delay.h>
 #include <avr/interrupt.h>
-#include <avr/pgmspace.h>
+#include <util/delay.h>
+#include <stdio.h>
+#include "myDS1307RTC.h"
+#define PIN_SCK                   1 // CKL
+#define PIN_MOSI                  2 // DIN
+#define PIN_SS                    0 // LOAD
 
-#define DS1307_SECONDS_ADDR		0x00
-#define DS1307_MINUTES_ADDR		0x01
-#define DS1307_HOURS_ADDR		0x02
-#define DS1307_DAY_ADDR			0x03
-#define DS1307_DATE_ADDR		0x04
-#define DS1307_MONTH_ADDR		0x05
-#define DS1307_YEAR_ADDR		0x06
-#define DS1307_CONTROL_ADDR		0x07
+#define PIN_SCL						PD0
+#define PIN_SDA						PD1
+#define DS1307_BASE_ADDRESS 		0xD0
 
-unsigned char hour, minute, second, day, date, month, year, control;
-unsigned char code_led[12] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90, 0xff, 0x00};
-unsigned char led_buffer[16];
-unsigned char kt = 0, a = 0, d = 0, scan_led, counter;
-bit set0,set1,up1,up0,down0,down1;
-//****************CAC CHUONG CHINH CON***********
-void timer0(void)
+// External crystal frequency
+#define F_RTC           32768
+
+// Define delay
+#define PERIOD_MS       1000
+
+#define MAX7219_LOAD1             PORTB &= ~(1<<PIN_SS) // choose mode
+#define MAX7219_LOAD0             PORTB |= (1<<PIN_SS)
+
+#define MAX7219_MODE_DECODE       0x09 // decode BCD
+#define MAX7219_MODE_INTENSITY    0x0A // light
+#define MAX7219_MODE_SCAN_LIMIT   0x0B //
+#define MAX7219_MODE_POWER    	  0x0C // have two mode 0 or 1
+
+#define MAX7219_MODE_TEST         0x0F
+#define MAX7219_MODE_NOOP         0x00
+
+// address of register
+#define MAX7219_DIGIT0            0x01
+#define MAX7219_DIGIT1            0x02
+#define MAX7219_DIGIT2            0x03
+#define MAX7219_DIGIT3            0x04
+#define MAX7219_DIGIT4            0x05
+#define MAX7219_DIGIT5            0x06
+#define MAX7219_DIGIT6            0x07
+#define MAX7219_DIGIT7            0x08
+
+#define MAX7219_CHAR_BLANK        0x0F
+#define MAX7219_CHAR_NEGATIVE     0x0A
+
+volatile int16_t Second;
+volatile int16_t Minute;
+volatile int16_t Hour;
+volatile int16_t Date;
+volatile int16_t Month;
+volatile int16_t Year;
+
+void spiSend_Byte (char databyte)
 {
-	EA = 0;//cam ngat toan cuc
-	TMOD=0x2;//timer0 che do 8 bit tu dong nap lai
-	TL0=0x6;//Gia tri bat dau dem = 6 (tre 250us)
-	TH0=0x6;//gia tri TH	dc nap vao TL (che do 8 bit)
-	ET0=1;//cho phep ngat timer0
-	EA=1;//cho phep ngat toan cuc
-	TR0=1;//Bat dau chay timer0
+	// Copy data into the SPI data register
+	SPDR = databyte;
+	// Wait until transfer is complete
+	while (!(SPSR & (1 << SPIF)));
 }
 
-void quet_led(void)interrupt 1 //dia chi vecto ngat timer0
+void MAX7219_Write_Data(uint8_t data_register, uint8_t data)
 {
-	TR0=0;//dung chay Timer
-	if(++counter == 3)//sau 12x250us=3ms thi thay doi hien thi led
-	{
-		if(scan_led <= 7)//chan enb cua P2 =>0 khong  co led nao duoc hien thi
-		{
-			P2 = scan_led;//hien thi cac led o hang 1 (gio, phut,giay,thu)
-			P0 = code_led[(led_buffer[scan_led])];
-			data_time = 1;//hien thi gio phut giay
-		}
-		else
-		{
-				P2 = scan_led-0x8;// hien thi led hang 2 (ngay thang nam)
-				P0 = code_led[(led_buffer[scan_led])];
-				data_date = 1;//hien thi ngay thang nam
-			}
-		scan_led++;
-		if(scan_led >= 16) //vuot qua 16 led thi tro ve ban dau
-		{
-			scan_led = 0;
-		}
-		counter = 0;
-	}
-	TR0 = 1;//Chay TIMER0
+	MAX7219_LOAD0;
+	// Send the register where the data will be stored
+	spiSend_Byte(data_register);
+	// Send the data to be stored
+	spiSend_Byte(data);
+	MAX7219_LOAD1;
 }
 
-void delays(int time_delay)
+//dinh nghia bien chung
+//dinh nghi cac bien thoi gian
+//volatile uint8_t	Second=59, Minute=59, Hour=11,
+//		Day=1, Date=31, Month=12, Year=9, Mode=1, AP=1;
+//Mode: chon che do 12h hoac 24h, Mode nam o bit 6 cua thanh ghi HOURS,
+//Mode=1: 12H, Mode=0: 24H
+//AP: bien chi AM hoac PM trong Mode 12h, AP nam o bit 5 cua thang ghi HOURS,
+//AP=1:PM, AP=0: AM
+
+volatile  uint8_t tData[7], Time_count=0; //dinh nghia cac bien tam
+char dis[5];		//bien dis dung de luu tru string hien thi len LCD
+
+// doi BCD sang thap phan va nguoc lai------------
+uint8_t BCD2Dec(uint8_t BCD)
 {
-	unsigned int temp;
-	while(time_delay)
-	{
-		temp=1000;
-		while(temp)
-		{
-			temp--;
-		}
-		time_delay--;
-	}
+	uint8_t L, H;
+	L=BCD & 0x0F;
+	H=(BCD>>4)*10;
+	return (H+L);
+}
+uint8_t Dec2BCD(uint8_t Dec){
+	uint8_t L, H;
+	L=Dec % 10;
+	H=(Dec/10)<<4;
+	return (H+L);
+}
+//------------------------------------------------
+void Display_7seg (void)
+{
+	Second 	= BCD2Dec(tData[0] & 0x7F);
+	Minute 	= BCD2Dec(tData[1]);
+
+	Hour = BCD2Dec(tData[2] & 0x1F); //mode 12h
+
+
+	Date   	= BCD2Dec(tData[4]);
+	Month	= BCD2Dec(tData[5]);
+	Year	= BCD2Dec(tData[6]);
+
+// 	MAX7219_Write_Data(MAX7219_DIGIT7,(tData[2]/10));
+// 	MAX7219_Write_Data(MAX7219_DIGIT6,(tData[2]%10));
+// 
+// //	MAX7219_writeData(MAX7219_DIGIT2,(??)); dau -
+// 
+// 	MAX7219_Write_Data(MAX7219_DIGIT4,(tData[1]/10));
+// 	MAX7219_Write_Data(MAX7219_DIGIT3,(tData[1]%10));
+// 
+// //	MAX7219_writeData(MAX7219_DIGIT5,(??)); // dau -
+// 
+// 	MAX7219_Write_Data(MAX7219_DIGIT1,(tData[0]/10));
+// 	MAX7219_Write_Data(MAX7219_DIGIT0,(tData[0]%10));
+	MAX7219_Write_Data(MAX7219_DIGIT7,(Hour/10));
+	MAX7219_Write_Data(MAX7219_DIGIT6,(Hour%10));
+
+	//	MAX7219_writeData(MAX7219_DIGIT2,(??)); dau -
+
+	MAX7219_Write_Data(MAX7219_DIGIT4,(Minute/10));
+	MAX7219_Write_Data(MAX7219_DIGIT3,(Minute%10));
+
+	//	MAX7219_writeData(MAX7219_DIGIT5,(??)); // dau -
+
+	MAX7219_Write_Data(MAX7219_DIGIT1,(Second/10));
+	MAX7219_Write_Data(MAX7219_DIGIT0,(Second%10));
 }
 
-void writeds()
-{
-	rtc_write(0x00, second);
-	rtc_write(0x01, minute);
-	rtc_write(0x02, hour);
-	rtc_write(0x03, day);
-	rtc_write(0x04, date);
-	rtc_write(0x05, month);
-	rtc_write(0x06, year);
+void Display(void)
+{ //chuong trinh con  hien thi thoi gian doc tu DS1307
+//	Second 	= BCD2Dec(tData[0] & 0x7F);
+//	Minute 	= BCD2Dec(tData[1]);
+//
+//	if (Mode !=0) 	Hour = BCD2Dec(tData[2] & 0x1F); //mode 12h
+//	else 		  	Hour = BCD2Dec(tData[2] & 0x3F); //mode 24h
+//
+//	Date   	= BCD2Dec(tData[4]);
+//	Month	= BCD2Dec(tData[5]);
+//	Year	= BCD2Dec(tData[6]);
+//
+////	clr_LCD();		//xoa LCD
+////	//in gio:phut:giay
+////	print_LCD("Time: ");
+//	sprintf(dis, "%i",Hour);
+//	move_LCD(1,7);  print_LCD(dis); move_LCD(1,9); putChar_LCD('-');
+//	sprintf(dis, "%i",Minute);
+//	move_LCD(1,10); print_LCD(dis); move_LCD(1,12);putChar_LCD('-');
+//	sprintf(dis, "%i",Second);
+//	move_LCD(1,13); print_LCD(dis);
+//	if (Mode !=0){ //mode 12h
+//		move_LCD(1,16);
+//		if (bit_is_set(tData[2],5))  putChar_LCD('P'); //kiem tra bit AP, if AP=1
+//		else putChar_LCD('A');
+//	}
+//	//in nam-thang-ngay
+//	move_LCD(2,1);
+//	print_LCD("Date: ");
+//	sprintf(dis, "%i",Year);
+//	move_LCD(2,7);
+//	if (Year<10) putChar_LCD('0'); // neu nam <10, in them so 0 ben trai, vi du 09
+//	print_LCD(dis); move_LCD(2,9); putChar_LCD('-'); //in Nam
+//	sprintf(dis, "%i",Month);
+//	move_LCD(2,10); print_LCD(dis); move_LCD(2,12);putChar_LCD('-'); //in thang
+//	sprintf(dis, "%i",Date);
+//	move_LCD(2,13); print_LCD(dis);	//in Ngay
 }
 
-void readds()
-{
-	if(kt == 0)
-	{
-		if(a == 1)
-		{
-			writeds();
-			a = 0;
-		}
-		second = rtc_read(0x00);
-		minute = rtc_read(0x01);
-		hour = rtc_read(0x02);
-		day = rtc_read(0x03);
-		date = rtc_read(0x04);
-		month = rtc_read(0x05);
-		year = rtc_read(0x06);
-	}
-}
-
-void dislay()
-{
-		unsigned char temp;
-
-		//*******Hien thi gio*************
-		if((temp=(hour&0x30)>>4)==0)//khong hien thi so 0 o hang chuc
-		{
-			led_buffer[0]=0x0a;
-		}
-		else
-		 {
-			led_buffer[0]=temp;//Lay 2 bit cao
-		 }
-		led_buffer[1]=(hour&0x0f);	 //Lay 4 bit thap
-		//*******Hien thi phut**********
-		led_buffer[2]=(minute&0x70)>>4; //lay 3 bit cao
-		led_buffer[3]=minute&0x0f;		 //Lay 4 bit thap
-		//*******Hien thi giay*********
-		led_buffer[4]=(second&0x70)>>4;
-		led_buffer[5]=second&0x0f;
-		led_buffer[6]=0x0a;
-		led_buffer[7]=(day&0x7);
-		//*******Hien thi ngay**********
-		if((temp=(date&0x70)>>4)==0)//khong hien thi so 0 o hang chuc
-		{
-			led_buffer[8]=0x0a;
-		}
-		else
-		 {
-			led_buffer[8]=temp;
-		 }
-		led_buffer[9]=date&0x0f;
-		//*********Hien thi thang************
-		if(((month&0x30)>>4)==0)//khong hien thi so 0 o hang chuc
-		{
-			led_buffer[10]=0x0a;
-		}
-		else
-			{
-				led_buffer[10]=(month&0x30)>>4;
-			}
-		led_buffer[11]=month&0x0f;
-		//******Hien thi nam***********
-		led_buffer[12]=2;
-		led_buffer[13]=0;
-		led_buffer[14]=(year&0x70)>>4;
-		led_buffer[15]=year&0x0f;
-
-}
-
-void setup_rtc(void)
-{
-				second=0x0;
-				minute=0x0;
-				hour=0x0;
-				day=0x4;
-				date=0x8;
-				month=0x6;
-				year=0x11;
-				control=0x10;
-	rtc_write(0x7, control);
-	rtc_write(0x0, second);
-	rtc_write(0x1, minute);
-	rtc_write(0x2, hour);
-	rtc_write(0x3, day);
-	rtc_write(0x4, date);
-	rtc_write(0x5, month);
-	rtc_write(0x6, year);
-}
-
-void ktphim()
-{
-set0=set1;set1=set;
-if((set0==1)&&(set1==0))
-  {
-   	if(set==0)
-	{
-		delays(50);
-		if((set==0)&&(clear==0))
-		{
-			delays(50);
-			setup_rtc();
-		}
-   	}
-   kt++;
-   if(kt==8)
-	   {
-	   	   kt=0;
-	   }
-  }
-if(clear==0)
-	{
-		kt=0;
-	}
-switch(kt)
- {
-  case 0:
-  {
-	  ;
-  }
-	 break;
-  case 1:
-    {
-      up0=up1;up1=up; a=1;d=1;
-      if((up0==1)&&(up1==0))
-       {
-         hour++;
-		  if(hour==24)hour=0;
-       }
-       down0=down1;down1=down;
-       if((down0==1)&&(down1==0))
-        {
-          hour--;
-		   if(hour==-1)hour=23;
-        }
-     break;
-   }
-  case 2:
-    {
-     up0=up1;up1=up; a=1;d=1;
-     if((up0==1)&&(up1==0))
-       {
-         minute++;
-		 if(minute==60)
-		 minute=0;
-       }
-     down0=down1;down1=down;
-     if((down0==1)&&(down1==0))
-       {
-         minute--;
-		 if(minute==-1)
-		 minute=59;
-       }
-      break;
-    }
-
- case 3:
-    {
-     up0=up1;up1=up;a=1;d=1;
-     if((up0==1)&&(up1==0))
-      {
-       second++;
-		 if(second==60)
-		 second=0;
-      }
-      down0=down1;down1=down;
-     if((down0==1)&&(down1==0))
-      {
-      second--;
-	  if(second==-1)
-	  second=0;
-      }
-     break;
-    }
-case 4:
-    {
-      up0=up1;up1=up; a=1;d=1;
-      if((up0==1)&&(up1==0))
-       {
-         day++;
-		  if(day==8)day=1;
-       }
-       down0=down1;down1=down;
-       if((down0==1)&&(down1==0))
-        {
-          day--;
-		   if(day==0)
-		   day=7;
-        }
-     break;
-   }
-case 5:
-    {
-
-	  up0=up1;up1=up; a=1;d=1;
-      if((up0==1)&&(up1==0))
-       {
-	   	  date++;
-         	if((month==4)||(month==6)||(month==9)||(month==11)){if(date==31)date=1;}
-			if((month==1)||(month==3)||(month==5)||(month==7)||(month==8)||(month==10)||(month==12)){if(date==32)date=1;}
-			if((month==2)&&(year%4==0)){if(date==29)date=1;}
-			if((month==2)&&(year%4!=0)){if(date==30)date=1;}
-       }
-       down0=down1;down1=down;
-       if((down0==1)&&(down1==0))
-        {
-        date--;
-			if((month==4)||(month==6)||(month==9)||(month==11)){if(date==0)date=30;}
-			if((month==1)||(month==3)||(month==5)||(month==7)||(month==8)||(month==10)||(month==12)){if(date==0)date=31;}
-			if((month==2)&&(year%4==0)){if(date==0)date=28;}
-			if((month==2)&&(year%4!=0)){if(date==0)date=29;}
-		 }
-     break;
-   }
-case 6:
-    {
-      up0=up1;up1=up; a=1;d=1;
-      if((up0==1)&&(up1==0))
-       {
-         month++;
-		  if(month==13)month=1;
-       }
-       down0=down1;down1=down;
-       if((down0==1)&&(down1==0))
-        {
-          month--;
-		   if(month==0)month=12;
-        }
-     break;
-   }
-case 7:
-    {
-      up0=up1;up1=up; a=1;d=1;
-      if((up0==1)&&(up1==0))
-       {
-         year++;
-		  if(year==100)year=0;
-       }
-       down0=down1;down1=down;
-       if((down0==1)&&(down1==0))
-        {
-          	year--;
-		   	if(year==-1)year=99;
-        }
-     break;
-   }
-
-void chinhgio(void)
-{
-		if(kt==1)
-		{
-			if(sqw==1)
-			{
-				led_buffer[0]=hour/10;
-				led_buffer[1]=hour%10;
-			}
-			else if(sqw==0)
-				{
-					led_buffer[0]=10;
-					led_buffer[1]=10;
-				}
-		 }
-
-		if(kt==2)
-		{
-			if(sqw==1)
-			{
-				led_buffer[2]=minute/10;
-				led_buffer[3]=minute%10;
-			}
-			else if(sqw==0)
-				{
-					led_buffer[2]=10;
-					led_buffer[3]=10;
-				}
- 		}
-		if(kt==3)
-		{
-			if(sqw==1)
-			{
-				led_buffer[4]=second/10;
-				led_buffer[5]=second%10;
-			}
-			else if(sqw==0)
-				{
-					led_buffer[4]=10;
-					led_buffer[5]=10;
-				}
-		}
-		if(kt==4)
-		{
-			if(sqw==1)
-			{
-				led_buffer[6]=10;
-				led_buffer[7]=day%10;
-			}
-			else if(sqw==0)
-				{
-					led_buffer[6]=10;
-					led_buffer[7]=10;
-				}
-		}
-		if(kt==5)
-		{
-			if(sqw==1)
-			{
-				led_buffer[8]=date/10;
-				led_buffer[9]=date%10;
-			}
-			else if(sqw==0)
-				{
-					led_buffer[8]=10;
-					led_buffer[9]=10;
-				}
- 		}
-		if(kt==6)
-		{
-			if(sqw==1)
-			{
-				led_buffer[10]=month/10;
-				led_buffer[11]=month%10;
-			}
-			else if(sqw==0)
-				{
-					led_buffer[10]=10;
-					led_buffer[11]=10;
-				}
-		}
-		if(kt==7)
-		{
-			if(sqw==1)
-			{
-				led_buffer[12]=2;
-				led_buffer[13]=0;
-				led_buffer[14]=year/10;
-				led_buffer[15]=year%10;
-			}
-			else if(sqw==0)
-				{
-					led_buffer[12]=10;
-					led_buffer[13]=10;
-					led_buffer[14]=10;
-					led_buffer[15]=10;
-				}
-		}
-}
-
-void khoi_chay()
-{
-	P0 = 0x00;
-	delays(100);
-	led_buffer[0]=0xa;
-	_delay_ms(5000);
-	led_buffer[1]=0xa;
-	_delay_ms(5000);
-	led_buffer[2]=0xa;
-	_delay_ms(5000);
-	led_buffer[3]=0xa;
-	_delay_ms(5000);
-	led_buffer[4]=0xa;
-	_delay_ms(5000);
-	led_buffer[5]=0xa;
-	_delay_ms(5000);
-	led_buffer[6]=0xa;
-	_delay_ms(5000);
-	led_buffer[7]=0xa;
-	_delay_ms(5000);
-	led_buffer[8]=0xa;
-	_delay_ms(5000);
-	led_buffer[9]=0xa;
-	_delay_ms(5000);
-	led_buffer[10]=0xa;
-	_delay_ms(5000);
-	led_buffer[11]=0xa;
-	_delay_ms(5000);
-	led_buffer[12]=0xa;
-	_delay_ms(5000);
-	led_buffer[13]=0xa;
-	_delay_ms(5000);
-	led_buffer[14]=0xa;
-	_delay_ms(5000);
-	led_buffer[15]=0xa;
-	_delay_ms(5000);
-}
-
+//chuong trinh chinh
 int main(void)
 {
-	timer0();
-	khoi_chay();
+
+	DDRB |= (1 << PIN_SCK)| (1 << PIN_SS)| (1 << PIN_MOSI);
+	SPCR |=(1<<SPE) |(1<<MSTR)| (1<<SPR1);
+	// DS1307 DATA DIRECTION
+	DDRD |= (1 << PIN_SCL)| (1 << PIN_SDA);
+	PORTD &= ~(1 << PIN_SCL)| (1 << PIN_SDA);
+
+	MAX7219_Write_Data(MAX7219_MODE_DECODE, 0xFF);
+	MAX7219_Write_Data(MAX7219_MODE_INTENSITY, 0x0A);
+	MAX7219_Write_Data(MAX7219_MODE_SCAN_LIMIT, 0x07);
+	MAX7219_Write_Data(MAX7219_MODE_POWER, 0x01);
+	
+	//khoi dong Timer0 lam bo dinh thi 1s------------------------------------------------
+	TCCR0=(1<<CS02)|(0<<CS01)|(1<<CS00);	//CS02=1, CS01=0, CS00=1: chon Prescaler=1024
+    TIMSK=(1<<TOIE0);						//cho phep ngat khi co tran o T/C0
+    sei();                      			//set bit I cho phep ngat toan cuc
+	//----------------------------------------------------------------
+
+	//khoi dong gia tri ghi vao DS1307---------------------------------------------------
+	tData[0]=Dec2BCD(Second);
+	tData[1]=Dec2BCD(Minute);
+	
+	tData[2]=Dec2BCD(Hour);
+/*	tData[3]=Dec2BCD(Day);*/
+	tData[4]=Dec2BCD(Date);
+	tData[5]=Dec2BCD(Month);
+	tData[6]=Dec2BCD(Year);
+	TWI_Init(); //khoi dong TWI
+	TWI_DS1307_wblock(0x00, tData, 7); //ghi lien tiep cac bien thoi gian vao DS1307
+	//----------------------------------------------------------------------------------
+	_delay_ms(1);	//cho DS1307 xu li
+	Display_7seg(); // hien thi ket qua len LCD
+	//doc va hien thi thoi gian lan dau tien******************************************
+	TWI_DS1307_wadr(0x00); //set dia chi ve 0
+	_delay_ms(1);			//cho DS1307 xu li
+	TWI_DS1307_rblock(tData,7); //doc ca khoi thoi gian (7 bytes)
+	
+	//************************************************************************************
+
 	while(1)
-  	{
-   		ktphim();
-   		readds();
-		if(d==0)
-   		{
-			dislay();
-   		}
-   		if(d==1)
-   		{
-			chinhgio();
-			d=0;
-   		}
-  	}
+	{
+		
+	}
+	return 0;
+}
+
+ISR (TIMER0_OVF_vect )
+{
+	Time_count++;
+	if(Time_count>=10)
+	{
+		//doc DS1307
+		TWI_DS1307_wadr(0x00); 				//set dia chi ve 0
+		_delay_ms(1);		   				//cho DS1307 xu li
+		TWI_DS1307_rblock(tData,7); 	//doc ca khoi thoi gian (7 bytes)
+		//hien thi ket qua len LCD
+		if(BCD2Dec(tData[0]) !=Second)
+		{ 	//chi hien thi ket qua khi da qua 1s
+			Second=BCD2Dec(Second & 0x7F);
+//			sprintf(dis, "%i",Second);
+//			move_LCD(1,13); print_LCD("  ");
+//			move_LCD(1,13); print_LCD(dis);
+// 			if (Second==0)
+// 				Display_7seg(); 		//moi phut cap nhat 1 lan
+		}
+		Time_count=0;
+	}
 }
